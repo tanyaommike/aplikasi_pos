3490a02e7e6834d35598222c29f2bb56deebf363
M1K3 <tanyaommike@gmail.com>
2026-07-21 09:23:01 +0700

    feat: tambah fitur search produk by nama serta menu navigasi produk

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,6 +22,12 @@
                         {{ __('Kategori') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('produk.index')" :active="request()->routeIs('produk.index')">
+                        {{ __('Produk') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
