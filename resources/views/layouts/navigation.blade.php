02b9f1f423b35d8fdc5b59c606e4ddb0ab378c60
tanyaommike <tanyaommike@gmail.com>
2026-07-22 09:38:30 +0700

    feat: tambah menu navigasi Transaksi

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -28,6 +28,12 @@
                         {{ __('Produk') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('transaksi.index')" :active="request()->routeIs('transaksi.index')">
+                        {{ __('Transaksi') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
